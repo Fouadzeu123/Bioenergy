b24982f2009b19e5d1222de396fab97343322be1
Fouadzeu <borisange81@gmail.com>
2026-04-15 14:50:27 +0100

    Fix withdrawal beneficiary channel

--- app/Services/NotchPayService.php
+++ app/Services/NotchPayService.php
@@ -261,6 +261,7 @@ class NotchPayService
                 ->post("{$this->apiUrl}/beneficiaries", [
                     'name'           => $name,
                     'phone'          => $phone,
+                    'number'         => $phone,
                     'email'          => $email,
                     'channel'        => $channel,
                     'country'        => $country,
